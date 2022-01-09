835e4f928fd55582d71ee2dcaf6946f29dcc2ee1
eejai42 <ej@ssot.me>
2022-01-08 18:04:15 -0600

    Day 2 completed.  The last step was actually just finishing part 2 of day1. :)

---
+++ Days/Day2/Day2Tests/Submarine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Day2Tests
+{
+    internal class Submarine
+    {
+        public enum Commands
+        {
+            Forward,
+            Up,
+            Down
+        }
+        public Submarine()
+        {
+        }
+
+        public Vector3 Position { get; set; }
+
+        internal void CalculatePosition(List<String> input)
+        {
+            input.ForEach(command => this.ProcessCommand(command));
+        }
+
+        private void ProcessCommand(string command)
+        {
+            var parts = $"{command}".Split(" ");
+            var distance = Int32.Parse(parts[1]);
+            switch (parts[0])
+            {
+                case "forward":
+                    this.Position = new Vector3(this.Position.X + distance, this.Position.Y, this.Position.Z);
+                    break;
+
+                case "up":
+                    this.Position = new Vector3(this.Position.X, this.Position.Y - distance, this.Position.Z);
+                    break;
+
+                case "down":
+                    this.Position = new Vector3(this.Position.X, this.Position.Y + distance, this.Position.Z);
+                    break;
+            }
+        }
+    }
+}
---
+++ Days/Day2/Day2Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Day2Tests
+{
+    public class Tests
+    {
+        internal Submarine Submarine { get; private set; }
+        public List<String>Input { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            this.Submarine = new Submarine();
+            this.SetupInput();
+        }
+
+        private void SetupInput()
+        {
+            this.Input = File.ReadAllText("../../../Input.txt")
+                               .Split(Environment.NewLine)
+                               .ToList();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            this.Submarine.CalculatePosition(this.Input);
+            var answer = this.Submarine.Position.X * this.Submarine.Position.Y;
+            Assert.IsTrue(answer == 1648020);
+        }
+    }
+}
