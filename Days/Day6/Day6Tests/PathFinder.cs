2983ca539fc599d3660ca09cd3fa715afb7b3d98
eejai42 <ej@ssot.me>
2022-01-08 23:37:19 -0600

    Created Day6 Project.

---
+++ Days/Day6/Day6Tests/PathFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day6Tests
+{
+    internal class PathFinder
+    {
+        private List<string> rawValues;
+
+        public PathFinder(List<string> rawValues)
+        {
+            this.rawValues = rawValues;
+        }
+
+        internal int FindShortestPath()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
