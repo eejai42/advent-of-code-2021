a9029d56084abdb5b4fd617df024b0b8fe799116
eejai42 <ej@ssot.me>
2022-01-10 03:06:17 -0600

    Day9 Part2 done.

--- Days/Day9/Day09Tests/Day9.cs
+++ Days/Day9/Day09Tests/Day9.cs
@@ -19,12 +19,28 @@ namespace Day09Tests
         }
 
         [Test]
-        public void Test1()
+        public void Part1()
         {
             var floorMapper = new FloorMapper(this.Lines);
             floorMapper.FindLowPoints();
             var risk = floorMapper.LowPoints.Sum(riskLevel => riskLevel + 1);
             Assert.IsTrue(risk == 526);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var floorMapper = new FloorMapper(this.Lines);
+            floorMapper.FindLowPoints();
+            floorMapper.CalculatBasins();
+            var bigBasins = floorMapper.BasinSizes
+                                    .OrderByDescending(size => size)
+                                    .ToList();
+
+
+
+            var risk = bigBasins[0] * bigBasins[1] * bigBasins[2];
+            Assert.IsTrue(risk == 1123524);
         }
     }
 }
--- Days/Day9/Day09Tests/FloorMapper.cs
+++ Days/Day9/Day09Tests/FloorMapper.cs
@@ -10,6 +10,7 @@ namespace Day09Tests
         public FloorMapper(System.Collections.Generic.List<string> lines)
         {
             this.LowPoints = new List<int>();
+            this.BasinSizes = new List<int>();
             this.Lines = lines;
             this.Height = this.Lines.Count;
             this.Width = this.Lines[0].Length;
@@ -29,7 +30,50 @@ namespace Day09Tests
         public int Height { get; }
         public int Width { get; }
         public int[,] Map { get; }
+
+        internal void CalculatBasins()
+        {
+            this.BasinSizes.Clear();
+            for (var row = 0; row < this.Height; row++)
+            {
+                for (var col = 0; col < this.Width; col++)
+                {
+                    if (this.Map[col, row] == -1)
+                    {
+                        this.MarkBasin(col, row);
+                    }
+                }
+            }
+            this.WriteMap(this.FinalMap);
+        }
+
+        private void MarkBasin(int col, int row)
+        {
+            this.FinalMap[col, row] = 1;
+            var basinSize = this.MarkSpot(col, row);
+            this.BasinSizes.Add(basinSize);
+        }
+
+        private int MarkSpot(int col, int row)
+        {
+            var size = 0;
+            if ((col >= 0) && (col < this.Width) && (row >= 0) && (row < this.Height))
+            {
+                if ((this.FinalMap[col, row] < 9) && (this.FinalMap[col, row] >= 0))
+                {
+                    size++;
+                    this.FinalMap[col, row] = -2;
+                    size += this.MarkSpot(col, row - 1);
+                    size += this.MarkSpot(col + 1, row);
+                    size += this.MarkSpot(col, row + 1);
+                    size += this.MarkSpot(col - 1, row);
+                }
+            }
+            return size;
+        }
+
         public List<int> LowPoints { get; }
+        public List<int> BasinSizes { get; }
         public int[,] FinalMap { get; }
 
         internal void FindLowPoints()
@@ -43,6 +87,7 @@ namespace Day09Tests
                         var lowPoint = this.Map[col, row];
                         this.FinalMap[col, row] = -1;
                         this.LowPoints.Add(lowPoint);
+                        this.BasinSizes.Add(1);
                     }
                 }
             }
@@ -57,7 +102,7 @@ namespace Day09Tests
             {
                 for (var col = 0; col < this.Width; col++)
                 {
-                    sb.Append(map[col, row] == -1 ? 'X' : map[col, row].ToString()[0]);
+                    sb.Append(map[col, row] < 0 ? 'X' : map[col, row].ToString()[0]);
                 }
                 sb.AppendLine();
             }
